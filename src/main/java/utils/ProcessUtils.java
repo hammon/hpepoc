8bd5ec8c1b9711d31b939296bf31b3047656f11f
hammon <michael.alexandrov@gmail.com>
2016-05-25 22:27:33 +0300

    cleanup

--- src/main/java/utils/ProcessUtils.java
+++ src/main/java/utils/ProcessUtils.java
@@ -1,23 +1,17 @@
 package utils;
 
 import org.slf4j.LoggerFactory;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 
-/**
- * Created by malexan on 23/11/2014.
- */
+
 public class ProcessUtils {
 
     final static org.slf4j.Logger log = LoggerFactory.getLogger(ProcessUtils.class);
 
     public static void main(String args[]) {
         ProcessUtils proc = new ProcessUtils();
-
-       
-       // log.info("res:" + res);
     }
 
 
@@ -27,17 +21,13 @@ public class ProcessUtils {
         try {
             String line;
             Process p = Runtime.getRuntime().exec(cmd,null,workDir);
-            BufferedReader bri = new BufferedReader
-                    (new InputStreamReader(p.getInputStream()));
-            BufferedReader bre = new BufferedReader
-                    (new InputStreamReader(p.getErrorStream()));
+            BufferedReader bri = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader bre = new BufferedReader(new InputStreamReader(p.getErrorStream()));
             while ((line = bri.readLine()) != null) {
-                //log.info(line);
                 res += line + "\n";
             }
             bri.close();
             while ((line = bre.readLine()) != null) {
-                //log.info(line);
                 res += line  + "\n";
             }
             bre.close();
